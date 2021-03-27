920020ea741ff166cffb8c00fb8a774512a45653
yellowconvict <784743392@qq.com>
2021-03-27 11:50:46 +0800

    upgrade

---
+++ js/app.js
@@ -0,0 +1,120 @@
+const {app, BrowserWindow, Menu, Tray} = require('electron');
+const {Notification} = require('electron');
+const path = require('path');
+
+let mainWindow = null;
+let appTray = null;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    });
+    mainWindow.loadFile('index.html');
+    mainWindow.on('close', (e) => {
+        mainWindow.hide();
+        mainWindow.setSkipTaskbar(true);
+        e.preventDefault();
+    });
+    // mainWindow.webContents.openDevTools();
+}
+
+function buildTrayMenu() {
+    let trayTemplate = [
+        {label: 'Item3', type: 'radio', checked: true},
+        {label: 'Item4', type: 'radio'},
+        {
+            label: '关于',
+            click: () => {
+                let notification = {
+                    title: 'ConvBrowser Notification',
+                    body: process['env']['npm_package_description']
+                };
+                new Notification(notification).show();
+            }
+        },
+        {
+            label: '退出',
+            click: () => {
+                if (mainWindow != null) {
+                    mainWindow.destroy();
+                    mainWindow = null;
+                    app.quit();
+                }
+            }
+        }
+    ];
+    let trayMenu = Menu.buildFromTemplate(trayTemplate);
+    appTray = new Tray(path.join(__dirname, '../icon/app.ico'));
+    appTray.setToolTip('This is my application.');
+    appTray.setContextMenu(trayMenu);
+    appTray.on('click', () => {
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+            mainWindow.setSkipTaskbar(false);
+        } else {
+            mainWindow.show();
+            mainWindow.setSkipTaskbar(true);
+        }
+    });
+}
+
+function buildTopMenu() {
+    let template = [
+        {
+            label: "菜单",
+            submenu: [
+                {
+                    label: "打开新窗口",
+                    click: () => {
+                        let win = new BrowserWindow({width: 500, height: 500, webPreferences: {nodeIntegration: true}});
+                        win.loadFile("yellow.html");
+                        win.on("close", () => {
+                            win = null;
+                        })
+                    }
+                },
+                {
+                    label: "关闭",
+                    click: () => {
+                        console.log(mainWindow)
+                    }
+                }
+            ]
+        },
+        {
+            label: "帮助",
+            submenu: [
+                {label: "主页"},
+                {label: "关于"}
+            ]
+        }
+    ];
+    let menu = Menu.buildFromTemplate(template);
+    // Menu.setApplicationMenu(menu);
+    Menu.setApplicationMenu(null);
+}
+
+app.whenReady()
+    .then(createWindow)
+    .then(buildTopMenu)
+    .then(buildTrayMenu)
+;
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+});
+
